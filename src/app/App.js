c747c8fd71b17e8d1f35c6efd910b7a3cb98d8b9
Yufgeni <37274212+Yufcodex@users.noreply.github.com>
2018-12-29 01:40:15 -0600

    configurando frontend con webpack e interpretes de react (babel)

---
+++ src/app/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+	render(){
+		return (
+			<h1>Implementando FrontEnd con React1</h1>
+		)
+	}
+}
+
+
+export default App;
